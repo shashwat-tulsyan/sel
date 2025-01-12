6644cb28a22af375497260928ea71dcae5c66a8d
shashwat-tulsyan <shashwattulsyan123@gmail.com>
2025-01-12 23:12:54 +0530

    done

---
+++ src/test/java/day3/copy/CollageTest.java
@@ -0,0 +1,19 @@
+package day3.copy;
+
+import org.testng.annotations.Test;
+
+public class CollageTest {
+
+	@Test
+	public void collagetest1(){
+		System.out.println("collage test 1");
+	}
+	@Test
+	public void collagetest2(){
+		System.out.println("collage test 1");
+	}
+	@Test
+	public void collagetest3(){
+		System.out.println("collage test 1");
+	}
+}
---
+++ src/test/java/day3/copy/SchoolTest.java
@@ -0,0 +1,12 @@
+package day3.copy;
+
+import org.testng.annotations.Test;
+
+public class SchoolTest {
+	@Test
+	public void schooltest1(){
+		System.out.println("school test 1");
+	}
+	
+	
+}
